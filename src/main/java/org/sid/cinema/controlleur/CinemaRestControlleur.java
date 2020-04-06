5558a825a2fda2c0fcf8c36075704572aca92e0f
AbdouAzizDIOUF <abdouazizdiouf75@gmail.com>
2020-04-07 01:24:40 +0200

    definition de la methode getImage pour les films v4 [13-35]

--- src/main/java/org/sid/cinema/controlleur/CinemaRestControlleur.java
+++ src/main/java/org/sid/cinema/controlleur/CinemaRestControlleur.java
@@ -3,9 +3,14 @@ package org.sid.cinema.controlleur;
 import org.sid.cinema.Repository.FilmRepository;
 import org.sid.cinema.entity.Film;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 
 @RestController
@@ -17,8 +22,10 @@ public class CinemaRestControlleur {
         this.filmRepository = filmRepository;
     }
 
-    @GetMapping("/listeFilms")
-    public List<Film> films(){
-        return filmRepository.findAll();
+    @GetMapping(path = "/imageFilm/{id}", produces = MediaType.IMAGE_JPEG_VALUE)
+    public  byte[] getImage(@PathVariable("id") Long id) throws IOException {
+        Film f = filmRepository.findById(id).get();
+        return Files.readAllBytes(Paths.get(System.getProperty("user.home")+"/imagesProjects/l9-project/film/"+f.getPhoto()+".jpg"));
     }
+
 }
